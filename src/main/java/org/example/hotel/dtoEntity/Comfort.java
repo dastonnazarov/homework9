4d96ea0885a3bef2a49b6389c4e2d019217b445c
dastonnazarov <dastonnazarov0910@gmail.com>
2023-03-18 22:05:33 +0500

    field name add

--- src/main/java/org/example/hotel/dtoEntity/Comfort.java
+++ src/main/java/org/example/hotel/dtoEntity/Comfort.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -15,11 +16,22 @@ import javax.persistence.Table;
 @Table(name = "comfort")
 public class Comfort {
 
+    @Column(name = "basses")
     private String basses;
+
+    @Column(name = "sauna")
     private String sauna;
+
+    @Column(name = "karaoke")
     private String karaoke;
+
+    @Column(name = "smallBar")
     private String smallBar;
+
+    @Column(name = "airConditioner")
     private String airConditioner;
+
+    @Column(name = "tv")
     private String tv;
 
 
